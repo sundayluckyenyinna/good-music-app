aa44acabeea5aa8c242791512ca70e92b3a95838
luckydev <sundayluckyenyinnadeveloper@gmail.com>
2022-12-24 15:03:24 +0100

    Starting with good-music application

---
+++ src/main/java/com/goodmusic/goodmusicapp/GoodmusicappApplication.java
@@ -0,0 +1,13 @@
+package com.goodmusic.goodmusicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodmusicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodmusicappApplication.class, args);
+	}
+
+}
